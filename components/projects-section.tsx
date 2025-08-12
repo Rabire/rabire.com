dd129aebcdd2d81c0d2abc9901b46109251d5c45
Rabire <rabireh@outlook.fr>
2025-08-12 21:56:25 +0200

    up projects

--- components/projects-section.tsx
+++ components/projects-section.tsx
@@ -8,6 +8,7 @@ import {
   Home,
   Medal,
 } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 type Feature = {
@@ -23,47 +24,49 @@ const features: Feature[] = [
     icon: Home,
     title: "Ecotransac",
     description:
-      "Un réseau d'agent commerciaux indépendants en immobilier que j'ai co-fondé.",
+      "Un réseau d'agent commerciaux indépendants en immobilier que j'ai co-fondé. L'application Nestor est utilisé par une dizaine de technophiles tout les jours.",
     url: "https://recrutement.ecotransac.fr/outils",
-    image: "/images/ecotransac.webp", // TODO: add image
+    image: "/images/ecotransac.webp",
   },
   {
     icon: Medal,
     title: "Runswap",
     description:
-      "Une plateforme de revente en ligne de dossards de course que j'ai co-fondé aussi.",
+      "Une plateforme de revente en ligne de dossards de course que j'ai co-fondé aussi. Des milliers de visiteurs uniques chaque mois.",
     url: "https://runswap.co",
-    image: "/images/runswap.webp", // TODO: add image
+    image: "/images/runswap.webp",
   },
   {
     icon: Bot,
     title: "Beink Dream",
     description:
-      "Un outil qui permet de dessiner un croquis et de l'améliorer avec de l'IA.",
+      "Un outil qui permet de dessiner un croquis et de l'améliorer avec de l'IA. D'ailleurs on a présenter mon POC dans l'émission Qui veut être mon associé sur M6 !",
     url: "https://www.beink.fr",
-    image: "/images/beink.webp", // TODO: add image
+    image: "/images/beink.webp",
   },
   {
     icon: Dumbbell,
     title: "SPART",
-    description: "Une application mobile qui encourage le sport en entreprise.",
+    description:
+      "Une application mobile qui encourage le sport en entreprise. Une super DA et une appli qui tien debout comme la tour Eiffel !",
     url: "https://www.spart.life",
-    image: "/images/spart.webp", // TODO: add image
+    image: "/images/spart.webp",
   },
   {
     icon: Brain,
     title: "Neurexpo",
     description:
-      "Des mini-jeux autour des fonctions cognitives pour les étudiants.",
+      "Des mini-jeux autour des fonctions cognitives pour les étudiants. Imaginé, codé et publié en 2 jours.",
     url: "https://www.neurexpo.fr",
-    image: "/images/isitech.webp", // TODO: add image
+    image: "/images/neurexpo.webp",
   },
   {
     icon: ChartColumnIcon,
     title: "Devboard",
-    description: "Un outil de productivité pour les développeurs.",
+    description:
+      "Un outil de productivité pour les développeurs. Il s'agit d'un concours de dev sur Youtube (que j'ai gagné évidemment).",
     url: "https://devboard-murex.vercel.app",
-    image: "/images/devboard.webp", // TODO: add image
+    image: "/images/devboard.webp",
   },
 ];
 
@@ -92,7 +95,7 @@ const ProjectsSection = () => {
 
           return (
             <Link key={feature.title} href={feature.url} target="_blank">
-              <Card className="flex flex-col border rounded-xl overflow-hidden shadow-none pb-0 hover-card-border">
+              <Card className="group h-full flex flex-col border rounded-xl overflow-hidden shadow-none pb-0 hover-card-border">
                 <CardHeader>
                   <Icon className="h-6 w-6 text-primary" />
                   <h4 className="!mt-3 text-xl font-semibold tracking-tight">
@@ -104,7 +107,14 @@ const ProjectsSection = () => {
                 </CardHeader>
 
                 <CardContent className="mt-auto px-0 pb-0">
-                  <div className="bg-muted h-52 ml-6 rounded-tl-xl" />
+                  <div className="bg-muted aspect-video ml-6 rounded-tl-xl relative overflow-hidden">
+                    <Image
+                      src={feature.image}
+                      alt={feature.title}
+                      fill
+                      className="object-cover rounded-tl-xl group-hover:scale-105 transition-transform duration-300"
+                    />
+                  </div>
                 </CardContent>
               </Card>
             </Link>
